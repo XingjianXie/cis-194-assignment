06751105d625a539bee3da5d7d3da883cab48f43
Xingjian Xie <xiexingjian2003@gmail.com>
2021-07-01 12:13:04 +0800

    Finish exercise 2 & 3 in HW-4

--- week4/HW-4.hs
+++ week4/HW-4.hs
@@ -11,3 +11,40 @@ next x | even x    = x `div` 2
 
 fun2' :: Integer -> Integer
 fun2' x = sum (filter even (takeWhile (/=1) (iterate next x)))
+
+-- Exercise 2
+
+data Tree a = Leaf
+            | Node Integer (Tree a) a (Tree a)
+  deriving (Show, Eq)
+
+height :: Tree a -> Integer
+height Leaf = -1
+height (Node x _ _ _) = x
+
+insert :: a -> Tree a -> Tree a
+insert element Leaf = Node 0 Leaf element Leaf
+insert element (Node x left element' right)
+  | height left < height right = Node x (insert element left) element' right
+  | height left > height right = Node x left element' (insert element right)
+  | otherwise                  = Node (1 + max (height left) (height right'))
+                                      left element' right'
+                                      where right' = insert element right
+
+foldTree :: [a] -> Tree a
+foldTree = foldr insert Leaf
+
+-- Exercise 3
+
+xor :: [Bool] -> Bool
+xor = foldl (\a a' -> (a && not a') || (not a && a')) False
+
+map' :: (a -> b) -> [a] -> [b]
+map' f = foldr (\aElement bList -> f aElement:bList) []
+
+myFoldl :: (a -> b -> a) -> a -> [b] -> a
+-- This is an simple enough logic that we just reverse the xs
+-- But I believe this is not what you really want
+myFoldl f base xs = foldr (\x -> (`f` x)) base (reverse xs)
+
+
